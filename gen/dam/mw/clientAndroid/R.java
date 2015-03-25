a5382631b06404722922f3d1b7b211060b97ce58
miquel <miquel@miquel-PC>
2015-03-25 15:50:32 +0100

    Mejoras de diseño

--- gen/dam/mw/clientAndroid/R.java
+++ gen/dam/mw/clientAndroid/R.java
@@ -34,16 +34,17 @@ public final class R {
     public static final class drawable {
         public static final int bg_generic=0x7f020000;
         public static final int error=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int mago=0x7f020003;
-        public static final int minus=0x7f020004;
-        public static final int okey=0x7f020005;
-        public static final int pergamino=0x7f020006;
-        public static final int plus=0x7f020007;
-        public static final int rounded_edittext=0x7f020008;
-        public static final int separator=0x7f020009;
-        public static final int toggle_style=0x7f02000a;
-        public static final int warlock=0x7f02000b;
+        public static final int et_tf_black_no_focus=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int mago=0x7f020004;
+        public static final int minus=0x7f020005;
+        public static final int okey=0x7f020006;
+        public static final int pergamino=0x7f020007;
+        public static final int plus=0x7f020008;
+        public static final int rounded_edittext=0x7f020009;
+        public static final int separator=0x7f02000a;
+        public static final int toggle_style=0x7f02000b;
+        public static final int warlock=0x7f02000c;
     }
     public static final class id {
         public static final int LinearLayout2=0x7f09002b;
--- src/dam/mw/clientAndroid/controlCenter/CConstants.java
+++ src/dam/mw/clientAndroid/controlCenter/CConstants.java
@@ -3,7 +3,7 @@ package dam.mw.clientAndroid.controlCenter;
 public final class CConstants {
 
 	//public static final String HOST = "192.168.1.13"; //IP TAULE 172.
-	public static final String HOST = "172.16.253.68";
+	public static final String HOST = "172.16.253.134";
 	public static final int PORT = 4444;
 	
     public static final String SUCCES = "99"; //numero o text?¿?¿?
